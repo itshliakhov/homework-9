22c67aa20c4a1b0bb31c259b2e44c85d04f1d423
Daniil <110263140+itshliakhov@users.noreply.github.com>
2022-10-04 10:42:58 +0300

    Updated firstTask.js
    deleted unused const, changed some code.

--- firstTask.js
+++ firstTask.js
@@ -1,9 +1,8 @@
 /* 
 Есть блок с текстом на странице и кнопка. При нажатии на кнопку текст изменяет цвет. При повторном нажатии – возвращается предыдущий цвет
 */
-const block = document.querySelector(".block"),
-  blockText = document.querySelector(".block_text"),
-  changeColorBtn = document.querySelector(".changeColorBtn");
+  const blockText = document.querySelector(".block_text");
+  const changeColorBtn = document.querySelector(".changeColorBtn");
 changeColorBtn.addEventListener("click", () => {
   if (blockText.style.color === "red") {
     blockText.style.color = "";
